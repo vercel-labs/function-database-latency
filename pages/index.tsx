6d153c7127b619755543a098aab67accc0e475f3
Guillermo Rauch <rauchg@gmail.com>
2022-12-16 22:43:41 -0800

    add shopify

--- pages/index.tsx
+++ pages/index.tsx
@@ -277,30 +277,3 @@ function Code({ className = "", children }) {
     </code>
   );
 }
-
-function ExternalLink({ href, children }) {
-  return (
-    <a
-      href={href}
-      className="text-blue-500 hover:text-blue-800"
-      target="_blank"
-      rel="noreferrer"
-    >
-      {children}
-    </a>
-  );
-}
-
-function PlanetScaleIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      preserveAspectRatio="xMidYMid meet"
-      viewBox="0 0 256 256"
-    >
-      <path d="M256 128.044c-.024 70.657-57.299 127.932-127.956 127.956ZM128 0c51.977 0 96.719 30.98 116.765 75.483L75.483 244.765a127.791 127.791 0 0 1-20.636-11.715L159.897 128H128l-90.51 90.51C14.327 195.345 0 163.345 0 128C0 57.308 57.308 0 128 0Z" />
-    </svg>
-  );
-}
